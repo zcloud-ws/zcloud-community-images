90e6b3dea73deff4f136bef20db90b5e5ba01fb4
Edimar Cardoso <edimarlnx@gmail.com>
2023-05-12 08:22:57 -0300

    Including author for commit and stop build if error

--- meteor-tools/meteor-create-tags.sh
+++ meteor-tools/meteor-create-tags.sh
@@ -9,8 +9,9 @@ echo "SUFFIX_TAG: ${SUFFIX_TAG}"
 echo "${NEXT_VERSION}" > "${FILE_VERSION}"
 
 git add "${FILE_VERSION}"
-git commit -m "Create/Update meteor version. Internal version: ${NEXT_VERSION}" \
-  --author="zCloud <infra@zcloud.ws>"
+git config --global user.email "infra@zcloud.ws"
+git config --global user.name "zCloud"
+git commit -m "Create/Update meteor version. Internal version: ${NEXT_VERSION}"
 git tag "meteor-build-${SUFFIX_TAG}"
 git tag "meteor-node-runtime-${SUFFIX_TAG}"
 git tag "meteor-node-mongodb-runtime-${SUFFIX_TAG}"
