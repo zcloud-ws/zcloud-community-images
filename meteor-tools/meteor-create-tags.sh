a361c255bfc0e3094e75a8366e87ba874c0dde17
Edimar Cardoso <edimarlnx@gmail.com>
2023-05-12 08:15:51 -0300

    Including author for commit and stop build if error

--- meteor-tools/meteor-create-tags.sh
+++ meteor-tools/meteor-create-tags.sh
@@ -5,7 +5,9 @@ METEOR_VERSION="${1:-"2.12"}"
 FILE_VERSION=".versions/.current-version-${METEOR_VERSION}"
 NEXT_VERSION="$(../scripts/extract_next_version.sh "${FILE_VERSION}")"
 SUFFIX_TAG="${METEOR_VERSION}-v${NEXT_VERSION}"
+echo "SUFFIX_TAG: ${SUFFIX_TAG}"
 echo "${NEXT_VERSION}" > "${FILE_VERSION}"
+
 git add "${FILE_VERSION}"
 git commit -m "Create/Update meteor version. Internal version: ${NEXT_VERSION}" \
   --author="zCloud <infra@zcloud.ws>"
