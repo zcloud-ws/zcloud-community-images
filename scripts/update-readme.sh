0176879c5c7d1663ea98b59f41f1df2ef2923379
Edimar Cardoso <edimarlnx@gmail.com>
2023-08-03 11:36:32 -0300

    Update meteors images

--- scripts/update-readme.sh
+++ scripts/update-readme.sh
@@ -3,7 +3,7 @@ set -e
 
 TOKEN="$(curl -s -X POST https://hub.docker.com/v2/users/login \
      -H 'Content-Type: application/json' \
-     -d  "{\"username\":\"${ZC_USERNAME}\",\"password\":\"${ZC_TOKEN}\"}" | grep -Po 'ey.*[^"}]')"
+     -d  "{\"username\":\"${ZC_USERNAME}\",\"password\":\"${ZC_TOKEN}\"}" | grep -Po 'ey.*[^"}]' || echo '')"
 #| grep -Po '"token":.*?[^\\]",' || echo ''
 if [ "x${TOKEN}" != "x" ]; then
   curl -X PATCH https://hub.docker.com/v2/repositories/${IMAGE_REPO}/${IMAGE_NAME} \
