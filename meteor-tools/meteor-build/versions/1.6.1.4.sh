430b95e4ff292ebdadf116c6cc9ea7741d389a24
Edimar Cardoso <edimarlnx@gmail.com>
2023-10-30 01:10:15 -0300

    Update meteor scripts

---
+++ meteor-tools/meteor-build/versions/1.6.1.4.sh
@@ -0,0 +1,6 @@
+export IMAGE_REPO="zcloudws"
+export IMAGE_NAME="meteor-build"
+export IMAGE_TAG="1.6.1.4"
+export INIT_COMMAND_VARS="export NODE_TLS_REJECT_UNAUTHORIZED=0"
+export PACKAGES="build-essential python2"
+#export ENV_VARS="build-essential python2"
---
+++ meteor-tools/meteor-node-mongodb-runtime/versions/1.6.1.4.sh
@@ -0,0 +1,6 @@
+export IMAGE_REPO="zcloudws"
+export IMAGE_NAME="meteor-node-mongodb-runtime"
+export IMAGE_TAG="1.6.1.4"
+export INIT_COMMAND_VARS="export NODE_TLS_REJECT_UNAUTHORIZED=0"
+export PACKAGES="python2"
+#export ENV_VARS="build-essential python2"
