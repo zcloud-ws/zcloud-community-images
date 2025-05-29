1a3eb02e78ac77e09c5f8949d761b96f3fadb25c
Edimar Cardoso <edimarlnx@gmail.com>
2025-05-29 13:49:28 -0300

    Add GLPI Dockerized Application with Build and Release Workflow
    Introduce a Dockerfile to build a containerized GLPI application with PHP and Apache. Include startup scripts, security configurations, cron jobs, and database requirements. Add a GitHub Actions workflow for building and pushing images and a README file detailing usage and project information.

---
+++ glpi/README.md
@@ -0,0 +1,12 @@
+# GLPI
+
+This directory contains the source code for the GLPI.
+
+# About GLPI
+
+GLPI is a free IT asset management, issue tracking system, and service desk software.
+It is used to manage IT assets, track issues, and provide support to users.
+It is written in PHP and uses a MySQL database.
+For more information, see the [GLPI website](https://glpi-project.org/).
+
+_by [Quave Cloud](https://www.quave.cloud)_
---
+++ glpi/files/opt/startup.sh
@@ -0,0 +1,47 @@
+#!/bin/bash -e
+
+# Create config and files volume (sub)directories that are missing
+# and set ACL for www-data user
+dirs=(
+    "/var/glpi/config"
+    "/var/glpi/files"
+    "/var/glpi/files/_cache"
+    "/var/glpi/files/_cron"
+    "/var/glpi/files/_dumps"
+    "/var/glpi/files/_graphs"
+    "/var/glpi/files/_locales"
+    "/var/glpi/files/_lock"
+    "/var/glpi/files/_log"
+    "/var/glpi/files/_pictures"
+    "/var/glpi/files/_plugins"
+    "/var/glpi/files/_rss"
+    "/var/glpi/files/_sessions"
+    "/var/glpi/files/_tmp"
+    "/var/glpi/files/_uploads"
+    "/var/glpi/files/_inventories"
+)
+for dir in "${dirs[@]}"
+do
+    if [ ! -d "$dir" ]
+    then
+        mkdir "$dir"
+    fi
+    setfacl -m user:www-data:rwx,group:www-data:rwx "$dir"
+done
+
+# Set ACL for www-data user on marketplace directory
+setfacl -m user:www-data:rwx,group:www-data:rwx "/var/www/glpi/marketplace"
+
+# Run cron service.
+cron
+
+if [ "$REMOVE_INSTALL" == "true" ]; then
+    # Remove install directory if it exists.
+    if [ -d "/var/www/glpi/install" ]; then
+        rm -rf /var/www/glpi/install
+    fi
+fi
+
+# Run command previously defined in base php-apache Dockerfile.
+apache2-foreground
+
